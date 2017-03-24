e47cb538575838980fda410921f427c8d213b9c2
Tobias Koppers <tobias.koppers@googlemail.com>
2017-03-24 23:04:02 +0100

    fixes webpack/webpack#4563

--- test/ReplaceSource.js
+++ test/ReplaceSource.js
@@ -41,4 +41,31 @@ describe("ReplaceSource", function() {
 		resultListMap.map.sourcesContent.should.be.eql(resultMap.map.sourcesContent);
 		resultListMap.map.mappings.should.be.eql("AAAA;AACA;AAAA;AAAA;AAIA,KACA");
 	});
+
+	it("should replace multiple items correctly", function() {
+		var line1, line2;
+		var source = new ReplaceSource(
+			new OriginalSource([
+				line1 = "Hello",
+				line2 = "World!"
+			].join("\n"), "file.txt")
+		);
+		source.insert(0, "Message: ");
+		source.replace(2, line1.length + 4, "y A");
+		var resultText = source.source();
+		var resultMap = source.sourceAndMap({
+			columns: true
+		});
+		var resultListMap = source.sourceAndMap({
+			columns: false
+		});
+		resultText.should.be.eql("Message: Hey Ad!");
+		resultMap.source.should.be.eql(resultText);
+		resultListMap.source.should.be.eql(resultText);
+		resultListMap.map.file.should.be.eql(resultMap.map.file);
+		resultListMap.map.version.should.be.eql(resultMap.map.version);
+		resultListMap.map.sources.should.be.eql(resultMap.map.sources);
+		resultListMap.map.sourcesContent.should.be.eql(resultMap.map.sourcesContent);
+		resultListMap.map.mappings.should.be.eql("AAAA,cACA");
+	});
 });
